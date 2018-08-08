dcc8cc04a7f3139610f5e71cb8e44761328debe1
Wil Cornish <wilcoclahas@gmail.com>
2018-08-08 14:27:33 +0100

    adds CSS styling

--- src/views/country_info_view.js
+++ src/views/country_info_view.js
@@ -17,10 +17,12 @@ CountryInfoView.prototype.render = function(country) {
  const heading = this.createHeading(country);
  const infoList = this.createInfoList(country);
  const img = this.createImage(country);
+ const languages = this.createLanguageList(country.languages);
 
  this.container.appendChild(heading);
  this.container.appendChild(infoList);
  this.container.appendChild(img);
+ this.container.appendChild(languages);
 };
 
 CountryInfoView.prototype.createHeading = function(country) {
@@ -39,7 +41,7 @@ CountryInfoView.prototype.createImage = function(country) {
 CountryInfoView.prototype.createInfoList = function(country) {
  const infoList = document.createElement('ul');
 
- const liRegion= this.createLi(`Region ${country.region}`, infoList);
+ const liRegion= this.createLi(`Region: ${country.region}`, infoList);
 
  return infoList;
 };
@@ -48,6 +50,18 @@ CountryInfoView.prototype.createLi = function(textContent, ul) {
  const li = document.createElement('li');
  li.textContent = textContent;
  ul.appendChild(li);
+};
+
+CountryInfoView.prototype.createLanguageList = function (languages) {
+ const list = document.createElement('ul');
+
+ languages.forEach((language, index) => {
+   const listItem = document.createElement('li');
+   listItem.textContent = language.name;
+   list.appendChild(listItem);
+ });
+
+ return list;
 };
 
 module.exports = CountryInfoView;
